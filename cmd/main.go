5bb7de5862a9b5dc511dbbf44c66675d26d7404b
Matthias Wessendorf <mwessend@redhat.com>
2020-12-04 15:46:10 +0100

    Adding initial ContainerSource
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

---
+++ cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"github.com/matzew/khue/pkg/connector"
+)
+
+var (
+	hueBridge string
+	username  string
+	lights    string
+)
+
+func init() {
+	flag.StringVar(&hueBridge, "hueBridge", "", "URL of the Hue Bridge (CloudEvents)")
+	flag.StringVar(&username, "username", "", "account on the Brdige")
+}
+
+func main() {
+	flag.Parse()
+
+	ha := connector.NewHueAdapter(hueBridge, username)
+	ha.ObserveLightState("Sonos Lampe WZ")
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/matzew/khue
+
+go 1.14
+
+require (
+	github.com/amimof/huego v1.1.0
+	github.com/cloudevents/sdk-go/v2 v2.3.1
+)
